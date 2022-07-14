0bd2b3150a74fdfe8603608c1a37b55b687d5196
Aline Mozer <alinemozer@gmail.com>
2022-07-14 11:04:41 -0300

    alteraçoes

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-const menu = document.getElementById('menu')
-const menuList = document.querySelectorAll('li')
-const menuActive = document.querySelector('.mobile-btn')
-const nav = document.getElementById('nav')
+const menu = document.getElementById('menu');
+const menuList = document.querySelectorAll('li');
+const menuActive = document.querySelector('.mobile-btn');
+const nav = document.getElementById('nav');
 
-function toggleMenu(){
+function toggleMenu() {
     nav.classList.toggle('ativo')
     menuActive.classList.toggle('ativo')
-}
+};
 
-function closeMenuAfterClickedALink(){
-    menuList.forEach( (item) =>{
+function fecharMenuAposClique() {
+    menuList.forEach( (item) => {
         item.addEventListener('click', () => {
            nav.classList.remove('ativo')
             menuActive.classList.remove('ativo')
-        })
-    })   
+        });
+    });  
+};
+
+menu.addEventListener('click', toggleMenu);
+fecharMenuAposClique();
+
+const setaVoltar = document.querySelector('.seta-esquerda');
+const setaAvancar = document.querySelector('.seta-direita');
+const imagensPainel = document.querySelectorAll('.painel');
+let painelAtual = 0;
+let painelAnterior = 0;
+
+setaAvancar.addEventListener('click', () => {
+    painelAnterior = painelAtual;
+    painelAtual ++;
+
+    mostrarPainel();
+    esconderPainel();
+    desabilitarSeta();
+});
+
+setaVoltar.addEventListener('click', () => {
+    painelAnterior = painelAtual;
+    painelAtual --;
+
+    mostrarPainel();
+    esconderPainel();
+    desabilitarSeta();
+});
+
+function mostrarPainel() {
+    imagensPainel[painelAtual].classList.add('mostrar');
 }
 
-menu.addEventListener('click', toggleMenu)
+function esconderPainel() {
+    imagensPainel[painelAnterior].classList.remove('mostrar');
+}
 
-closeMenuAfterClickedALink()
+function desabilitarSeta() {
+    const primeiroPainel = 0;
+    const ultimoPainel = imagensPainel.length -1;
+    
+    painelAtual === primeiroPainel ? setaVoltar.classList.add('seta-desabilitada') : setaVoltar.classList.remove('seta-desabilitada');
+
+    painelAtual === ultimoPainel ? setaAvancar.classList.add('seta-desabilitada') : setaAvancar.classList.remove('seta-desabilitada');
+}
