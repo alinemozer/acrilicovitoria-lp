c0906455d86a4343ac4c00dafcae93860bc235a8
Aline Mozer <alinemozer@gmail.com>
2022-07-14 19:01:43 -0300

    alterações

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const menu = document.getElementById('menu');
 const menuList = document.querySelectorAll('li');
 const menuActive = document.querySelector('.mobile-btn');
 const nav = document.getElementById('nav');
+const logoClientes = document.querySelectorAll('.logo-painel')
+const painelClienteImagemLogo = document.querySelector('.painel')
 
 function toggleMenu() {
     nav.classList.toggle('ativo')
@@ -20,3 +22,8 @@ function fecharMenuAposClique() {
 menu.addEventListener('click', toggleMenu);
 fecharMenuAposClique();
 
+logoClientes.forEach( (item, index) => {
+    item.addEventListener('click', function(){
+       painelClienteImagemLogo.src = `./src/images/cliente${index}.jpeg`
+    })
+});
