ae1c8486de01a8c897e7623f8358727628539595
Aline Mozer <alinemozer@gmail.com>
2022-07-15 16:03:06 -0300

    alterações

---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+const menu = document.getElementById('menu');
+const menuList = document.querySelectorAll('li');
+const menuActive = document.querySelector('.mobile-btn');
+const nav = document.getElementById('nav');
+const logoClientes = document.querySelectorAll('.logo-painel');
+const painelClienteImagemLogo = document.querySelector('.painel');
+
+function toggleMenu() {
+    nav.classList.toggle('ativo')
+    menuActive.classList.toggle('ativo')
+};
+
+function fecharMenuAposClique() {
+    menuList.forEach( (item) => {
+       
+        item.addEventListener('click', () => {
+           
+            menuList.forEach((elem) =>{
+                if (elem.lastElementChild.classList.contains('selecionado')){
+                    elem.lastElementChild.classList.remove('selecionado')
+                }
+            });
+            item.lastElementChild.classList.add('selecionado')
+            nav.classList.remove('ativo')
+            menuActive.classList.remove('ativo')
+        });    
+    });  
+};
+
+menu.addEventListener('click', toggleMenu);
+fecharMenuAposClique();
+
+logoClientes.forEach( (item, index) => {
+    item.addEventListener('click', function(){
+        setTimeout(() => {
+            painelClienteImagemLogo.classList.add('transparente') 
+        }, 250)
+        setTimeout(() =>{
+            painelClienteImagemLogo.src = `./src/images/cliente${index}.jpeg`
+            painelClienteImagemLogo.classList.remove('transparente') 
+        }, 600)
+    })
+});
+
+ScrollReveal().reveal('.logo-cinza', { delay: 300 });
+ScrollReveal().reveal('.paragrafo', { delay: 500 });
+ScrollReveal().reveal('.fa-arrow-down', { delay: 800 });
+ScrollReveal().reveal('.logo-clientes', { delay: 300 });
+ScrollReveal().reveal('.paineis', { delay: 500 });
+ScrollReveal().reveal('.produto', { delay: 500 });
